e6c2529d6fa8efc45b7a02e67daa2b8f2f2fe408
s446683009 <446683009@qq.com>
2023-03-01 14:53:04 +0800

    尝试新的路由v2

---
+++ app/routes/apis.user.loginOut.tsx
@@ -0,0 +1,6 @@
+import { ActionArgs } from '@remix-run/node';
+import {auth} from '../utils/auth.server'
+
+export async function action({ request }: ActionArgs) {
+    await auth.logout(request, { redirectTo: "/login" });
+  };
